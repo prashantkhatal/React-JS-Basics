2178699a171d3b63c9e852741df2f0cc536b5130
Prashant Khatal <prashant.khatal21@gmail.com>
2017-05-06 16:22:53 +0530

    My first component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,22 @@
-console.log("Yehhh It's works");
+import React from "react";
+import reactDom from "react-dom";
+
+class MyElement extends React.Component {
+    render(){
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <h1>Title</h1>
+                        <p>
+                            This is My First element.<br/>
+                            Use React obj to specify urs tech component and reactDom to render it.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+reactDom.render(<MyElement/>, window.document.getElementById('MyId'));
