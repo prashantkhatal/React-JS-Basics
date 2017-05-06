91e7f7460c6d4fdcbdf966f18b1398eccb10d216
Prashant Khatal <prashant.khatal21@gmail.com>
2017-05-06 16:55:12 +0530

    Expression in reacts

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,9 +2,11 @@ import React from "react";
 
 export class Home extends React.Component {
     render(){
+        let name= 'Prashant';
+        name = "Mr. " + name;
         return(
             <div>
-                This is my Content.
+                This is my Content and My Name is {name}
             </div>
         );
     }
